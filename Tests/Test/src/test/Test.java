0d409db411c14ada2473b773e9c4ed3914f81ae9
RusiruS <rusiru719@gmail.com>
2013-10-22 16:20:47 +0530

    changed main methods parameter

--- Tests/Test/src/test/Test.java
+++ Tests/Test/src/test/Test.java
@@ -13,7 +13,7 @@ public class Test {
    /**
     * @param args the command line arguments
     */
-   public static void main(String[] args) {
+   public static void main(String[] argss) {
       // TODO code application logic here
    }
 }
